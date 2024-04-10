08ddb0dc7c9aadc064ec7dcdab632c3e7a3bafe5
Skrudj <ssinilov@gmail.com>
2024-04-10 20:19:56 +0300

    fix code style

--- src/main/java/mate/academy/rickandmorty/service/impl/ApiServiceImpl.java
+++ src/main/java/mate/academy/rickandmorty/service/impl/ApiServiceImpl.java
@@ -6,7 +6,6 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.Objects;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import mate.academy.rickandmorty.model.ApiResponse;
